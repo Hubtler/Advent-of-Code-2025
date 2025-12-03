e3431b8a4b0373fe5fc81b5064ef252a5889612a
Hubtler <Sascha_96@gmx.de>
2025-12-03 22:19:57 +0100

    replaced for by map

--- Day03.py
+++ Day03.py
@@ -11,25 +11,14 @@ def get_joltage( bank, N_turn_on ):
     if N_turn_on == 1:
         return max(bank)
     biggest_ind = np.argmax(bank[:-(N_turn_on-1)])
-    biggest_val = bank[biggest_ind]
-    return biggest_val * 10**(N_turn_on-1) + get_joltage(bank[ biggest_ind + 1: ], N_turn_on-1)
-
+    return bank[biggest_ind] * 10**(N_turn_on-1) + get_joltage(bank[ biggest_ind + 1: ], N_turn_on-1)
 
 def part1(input):
     battery_banks = parse(input)
-    total_joltage = 0
-    for bank in battery_banks:
-        joltage = get_joltage(bank, 2)
-        total_joltage += joltage
-    return total_joltage
-
+    return sum( map( lambda bank: get_joltage(bank, 2), battery_banks ) )
 
 def part2(input):
     battery_banks = parse(input)
-    total_joltage = 0
-    for bank in battery_banks:
-        joltage = get_joltage(bank, 12)
-        total_joltage += joltage
-    return total_joltage
+    return sum(map(lambda bank: get_joltage(bank, 12), battery_banks))
 
 aoca.auto_submit(part1, part2)
