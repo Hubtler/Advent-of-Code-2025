8436e1795cb55aa3bd2885dbc1bd4853d49fc46d
Hubtler <Sascha_96@gmx.de>
2025-12-07 21:50:10 +0100

    cleanup

--- Day06.py
+++ Day06.py
@@ -4,65 +4,51 @@ aoca = AOC_Auto(day=6, year=2025)
 
 def parse(text):
     lines = text.split("\n")
+    blocks = [list(map(lambda x: int(x), line.split())) for line in lines[:-1]]
+    operator_list = lines[-1].split()
+    return blocks, operator_list
 
-    def remove_double_empty_spaces(line):
-        while "  " in line:
-            line = line.replace("  ", " ")
-        return line
+def transpose(lines):
+    return "\n".join(["".join([line[ind] for line in lines]).strip() for ind in range(len(lines[0]))])
 
-    numbers = [list(map(lambda x: int(x), remove_double_empty_spaces(line).strip().split(" "))) for line in lines[:-1]]
-    operator_list = remove_double_empty_spaces(lines[-1]).strip().split(" ")
-    return numbers, operator_list
+def parse_colwise(text):
+    lines = text.split("\n")
+    operators = [c for c in lines[-1] if c != " "]
+    cols = transpose(lines[:-1])
+    blocks_str = cols.split("\n\n")
+    blocks = [[int(c) for c in block.split("\n")] for block in blocks_str]
+    return blocks, operators
+
 
 def part1(input):
-    numbers, operator_list = parse(input)
-    math_result =[0 if operator == "+" else 1 for operator in operator_list]
+    blocks, operator_list = parse(input)
+    total = 0
     for ind, operator in enumerate(operator_list):
+        result = 0
         if operator == "+":
-            for number_list in numbers:
-                math_result[ind] += number_list[ind]
+            for block in blocks:
+                result += block[ind]
         elif operator == "*":
-            for number_list in numbers:
-                math_result[ind] *= number_list[ind]
-    return sum(math_result)
-def part2(input):
-    line_ended = True
-    lines = input.split("\n")
-    results = []
-    for ind in range(len(lines[0])):
-        if line_ended:
-            numbers_col = []
-        line_ended = True
-        number = ""
-        for line in lines:
-            if not line[ind] in " +*":
-                line_ended = False
-                number += line[ind]
-            if line[ind] in "*+":
-                operator = line[ind]
-
-        if not line_ended:
-            numbers_col.append( int(number) )
-        else:
-            if operator == "*":
-                result = 1
-                for number in numbers_col:
-                    result *= number
-            elif operator == "+":
-                result = 0
-                for number in numbers_col:
-                    result += number
-            results.append(result)
-    if not line_ended:
-        if operator == "*":
             result = 1
-            for number in numbers_col:
-                result *= number
-        elif operator == "+":
-            result = 0
-            for number in numbers_col:
+            for block in blocks:
+                result *= block[ind]
+        total += result
+    return total
+
+def part2(input):
+    blocks, operators = parse_colwise(input)
+    total = 0
+    for block, operator in zip(blocks, operators):
+        result = 0
+        if operator == "+":
+            for number in block:
                 result += number
-        results.append(result)
-    return sum(results)
+        elif operator == "*":
+            result = 1
+            for number in block:
+                result *= number
+        total += result
+    return total
+
 
 aoca.auto_submit(part1, part2)
