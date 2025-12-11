622c7f0839ae37484cb8eb90e82974a0f937f169
Hubtler <Sascha_96@gmx.de>
2025-12-11 22:15:27 +0100

    cleanup

--- Day11.py
+++ Day11.py
@@ -3,7 +3,6 @@ import functools
 
 aoca = AOC_Auto(day=11, year=2025, skip_example_part_a=True, skip_example_part_b=True)
 
-
 def parse(text):
     name_connected = {}
     lines = text.split("\n")
@@ -12,33 +11,30 @@ def parse(text):
         name_connected[parts[0]] = parts[1].split(" ")
     return name_connected
 
+def get_solver(name_connected):
+    @functools.cache
+    def connections_to_out_through(name, visit_first = tuple()):
+        if len(visit_first) == 0:
+            if name == "out":
+                return 1
+        elif name in visit_first:
+            visit_first = tuple(n for n in visit_first if n != name)
+
+        if name not in name_connected:
+            return 0
+
+        return sum(connections_to_out_through(n, visit_first) for n in name_connected[name])
+    return connections_to_out_through
 
 def part1(input):
     name_connected = parse(input)
-    @functools.cache
-    def connections_to_out(name):
-        if name == "out":
-            return 1
-        if name not in name_connected:
-            return 0
-        return sum(connections_to_out(n) for n in name_connected[name])
+    connections_to_out = get_solver(name_connected)
     return connections_to_out('you')
 
 
 def part2(input):
     name_connected = parse(input)
-    @functools.cache
-    def connections_to_out_dacfft(name, dac_seen=False, fft_seen=False):
-        if dac_seen and fft_seen and name == "out":
-            return 1
-        if name not in name_connected:
-            return 0
-        if name == "dac":
-            dac_seen = True
-        if name == "fft":
-            fft_seen = True
-
-        return sum(connections_to_out_dacfft(n, dac_seen, fft_seen) for n in name_connected[name])
-    return connections_to_out_dacfft('svr')
+    connections_to_out = get_solver(name_connected)
+    return connections_to_out("svr", ("dac", "fft"))
 
 aoca.auto_submit(part1, part2)
